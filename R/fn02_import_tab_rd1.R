79e2dfe1906a15784bbbe763db65d4e71c5c5f19
ggwinter <49496645+ggwinter@users.noreply.github.com>
2022-03-15 15:15:49 +0100

    ajout nouvelles fonctions

---
+++ R/fn02_import_tab_rd1.R
@@ -0,0 +1,237 @@
+#' fn02_import_tab_rd1
+#'
+#' @param x le nom du repertoire ou est le fichier
+#' @importFrom attempt stop_if_not
+#' @importFrom dplyr across
+#' @importFrom dplyr all_of
+#' @importFrom dplyr case_when
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr pull
+#' @importFrom dplyr select
+#' @importFrom dplyr slice
+#' @importFrom dplyr starts_with
+#' @importFrom dplyr tibble
+#' @importFrom here here
+#' @importFrom janitor make_clean_names
+#' @importFrom purrr flatten_chr
+#' @importFrom purrr map_dfr
+#' @importFrom purrr set_names
+#' @importFrom readr write_csv
+#' @importFrom readxl excel_sheets
+#' @importFrom readxl read_xls
+#' @importFrom stats complete.cases
+#' @importFrom stringr str_c
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_extract
+#' @importFrom stringr str_replace
+#' @importFrom stringr str_replace_all
+#' @importFrom stringr str_squish
+#' @importFrom tidyr pivot_wider
+#' @importFrom tidyr replace_na
+#' @return nothing
+#' @export
+#'
+fn02_import_tab_rd1 <- function(x = "2_data") {
+
+  list.files(here::here(x), pattern = "_rd1_commercialisation") -> nom_fich
+
+  # teste la presence du fichier dans 2_data ou les doublons
+  #
+  attempt::stop_if_not(
+    .x = length(nom_fich),
+    .p = ~ .x < 2,
+    msg = "PB : il y a plusieurs fichiers rd1 dans 2_data\n"
+  )
+  attempt::stop_if_not(
+    .x = length(nom_fich),
+    .p = ~ .x > 0,
+    msg = "Pb : il n'y a pas de fichier rd1 dans 2_data\n"
+  )
+
+  # dernier trimestre du tableau
+  #
+  fich_rd1_trim <- stringr::str_extract(nom_fich, "[:digit:]{4}t[:digit:]")
+
+  cat(stringr::str_c("Le fichier rd1 du trimestre ", fich_rd1_trim, " va \u00eatre import\u00e9\n"))
+
+  # teste l ouverture du fichier notamment les lignes vides avant le tableau
+
+  vector(mode = "character", length = 0) -> fich_onglets
+  readxl::excel_sheets(here::here("2_data", nom_fich)) -> fich_onglets
+
+  readxl::read_xls(here::here("2_data", nom_fich), "94", col_names = FALSE) -> tab1
+
+  tab1 %>%
+    dplyr::pull(...1) %>%
+    stringr::str_which("ANNEE") -> lgn_annee
+
+  tab1 %>%
+    dplyr::pull(...1) %>%
+    stringr::str_which("Source") -> lgn_source
+
+
+  purrr::map(
+    c(lgn_annee, lgn_annee + 1, lgn_annee + 2),
+    ~ tab1 %>%
+      dplyr::slice(.x) %>%
+      purrr::flatten_chr()
+  ) %>%
+    purrr::map(., ~ stringr::str_replace_all(.x, "[\r\n]", " ")) -> ls_champs
+
+  t_champs <- dplyr::tibble(
+    lgn0 = ls_champs[[1]],
+    lgn1 = ls_champs[[2]],
+    lgn2 = ls_champs[[3]]
+  ) %>%
+    purrr::map_dfr(., ~ stringr::str_squish(.x))
+
+  t_champs %>%
+    tidyr::fill(lgn0, lgn1) %>%
+    dplyr::mutate(
+      lgn0 = dplyr::case_when(
+        stringr::str_detect(lgn0, "Maisons") ~ "Maisons",
+        stringr::str_detect(lgn0, "appartements") ~ "Appartements",
+        TRUE ~ lgn0
+      ),
+      lgn1 = tidyr::replace_na(lgn1, ""),
+      lgn2 = tidyr::replace_na(lgn2, ""),
+      champs = paste(lgn0, lgn1, lgn2, sep = "-") %>%
+        stringr::str_replace(., "-$|--$", "") %>% stringr::str_squish(),
+      champsp = janitor::make_clean_names(champs)
+    ) %>%
+    dplyr::select(champs, champsp) -> t_champs
+
+  t_champs_valides <- tibble::tribble(
+    ~champs, ~champsp, ~indic_cd,
+    "ANNEE", "annee", "ANNEE",
+    "TRIMESTRE", "trimestre", "TRIMESTRE",
+    "Appartements-Logts mis en vente au cours du trimestre-Total", "appartements_logts_mis_en_vente_au_cours_du_trimestre_total", "MEV_T_A",
+    "Appartements-Logts r\u00e9serv\u00e9s au cours du trimestre-Total", "appartements_logts_reserves_au_cours_du_trimestre_total", "RESV_T_A",
+    "Appartements-Encours de logts propos\u00e9s \u00e0 la vente \u00e0 la fin du trimestre-Total", "appartements_encours_de_logts_proposes_a_la_vente_a_la_fin_du_trimestre_total", "ENC_T_A",
+    "Appartements-Prix de vente en euros/m\u00b2 (1)-Total", "appartements_prix_de_vente_en_euros_m2_1_total", "PVMM2_T_A",
+    "Appartements-Encours de logts propos\u00e9s \u00e0 la vente \u00e0 la fin du trimestre-En projet", "appartements_encours_de_logts_proposes_a_la_vente_a_la_fin_du_trimestre_en_projet", NA,
+    "Appartements-Encours de logts propos\u00e9s \u00e0 la vente \u00e0 la fin du trimestre-En cours de construction", "appartements_encours_de_logts_proposes_a_la_vente_a_la_fin_du_trimestre_en_cours_de_construction", NA,
+    "Appartements-Encours de logts propos\u00e9s \u00e0 la vente \u00e0 la fin du trimestre-Achev\u00e9s", "appartements_encours_de_logts_proposes_a_la_vente_a_la_fin_du_trimestre_acheves", NA,
+    "Appartements-% des logts achev\u00e9s de l'encours total", "appartements_percent_des_logts_acheves_de_lencours_total", NA,
+    "Appartements-D\u00e9lai d'\u00e9coulement", "appartements_delai_decoulement", "DEC_T_A",
+    "Maisons-Logts mis en vente au cours du trimestre-Total", "maisons_logts_mis_en_vente_au_cours_du_trimestre_total", "MEV_T_M",
+    "Maisons-Logts r\u00e9serv\u00e9s au cours du trimestre-Total", "maisons_logts_reserves_au_cours_du_trimestre_total", "RESV_T_M",
+    "Maisons-Encours de logts propos\u00e9s \u00e0 la vente \u00e0 la fin du trimestre-Total", "maisons_encours_de_logts_proposes_a_la_vente_a_la_fin_du_trimestre_total", "ENC_T_M",
+    "Maisons-Prix de vente moyen en euros (1)-Total", "maisons_prix_de_vente_moyen_en_euros_1_total", "PVM_T_M",
+    "Maisons-D\u00e9lai d'\u00e9coulement", "maisons_delai_decoulement", "DEC_T_M"
+  )
+
+  # verification que la structure du fichier n a pas chang\u00e9e nombre et nom des colonnes
+
+  attempt::stop_if_not(
+    .x = length(which(!t_champs %>% dplyr::pull(champsp) %in% t_champs_valides$champsp)),
+    .p = ~ .x == 0,
+    msg = "Pb : la structure du fichier rd1 a chang\u00e9\n"
+  )
+
+  cat("Ok : La structure du fichier rd1 est inchang\u00e9e\n")
+
+  t_champs_valides %>%
+    dplyr::filter(stats::complete.cases(indic_cd)) -> t_champs_valides
+
+  which(t_champs %>%
+    dplyr::pull(champsp) %in% t_champs_valides$champsp) -> col_a_garder
+
+
+  # va ouvrir uniquement les onglets des regions
+
+  fich_onglets %>%
+    stringr::str_subset("[:digit:]{1,2}") %>%
+    purrr::set_names() -> fich_onglets_reg
+
+  purrr::set_names(
+    fich_onglets_reg,
+    stringr::str_c("R", fich_onglets_reg)
+  ) -> fich_onglets_reg
+
+
+  purrr::map(
+    fich_onglets_reg,
+    ~ readxl::read_xls(
+      here::here("2_data", nom_fich),
+      .x,
+      col_names = FALSE,
+      skip = lgn_annee + 3,
+      n_max = lgn_source - (lgn_annee + 4),
+      na = c("", "nd")
+    ) %>%
+      dplyr::select(dplyr::all_of(col_a_garder)) %>%
+      purrr::set_names(t_champs_valides$indic_cd) %>%
+      tidyr::fill(ANNEE) %>%
+      dplyr::mutate(
+        "ECLN_MEV_AG_T_A" = MEV_T_A,
+        "ECLN_MEV_AG_T_M" = MEV_T_M,
+        "ECLN_RESV_AG_T_A" = RESV_T_A,
+        "ECLN_RESV_AG_T_M" = RESV_T_M,
+        "TRIM_DAY" = dplyr::case_when(
+          stringr::str_detect(TRIMESTRE, "T1") ~ "03-31",
+          stringr::str_detect(TRIMESTRE, "T2") ~ "06-30",
+          stringr::str_detect(TRIMESTRE, "T3") ~ "09-30",
+          stringr::str_detect(TRIMESTRE, "T4") ~ "12-31",
+          TRUE ~ "Pb"
+        ),
+        "DATE" = paste(ANNEE, TRIM_DAY, sep = "-")
+      ) %>%
+      dplyr::mutate(dplyr::across(where(is.numeric), ~ tidyr::replace_na(.x, 0))) %>%
+      dplyr::mutate(dplyr::across(
+        c(
+          "MEV_T_A",
+          "MEV_T_M",
+          "RESV_T_A",
+          "RESV_T_M",
+          "ENC_T_A",
+          "ENC_T_M",
+          "ECLN_MEV_AG_T_A",
+          "ECLN_MEV_AG_T_M",
+          "ECLN_RESV_AG_T_A",
+          "ECLN_RESV_AG_T_M"
+        ),
+        as.integer
+      )) %>%
+      dplyr::mutate(dplyr::across(dplyr::starts_with("DEC_T"), ~ round(.x, 2))) %>%
+      dplyr::mutate(dplyr::across(c("PVMM2_T_A", "PVM_T_M"), ~ round(.x) %>%
+        as.integer())) %>%
+      dplyr::select(-TRIM_DAY)
+  ) -> ls_tab2
+
+  ls_tab2 %>%
+    purrr::set_names(stringr::str_replace(names(ls_tab2), "R", "")) -> ls_tab2
+
+
+  # prix des appartements par region
+  #
+  purrr::map_dfr(ls_tab2, ~ .x %>% dplyr::select(DATE, PVMM2_T_A), .id = "REG_CD") %>%
+    tidyr::pivot_wider(
+      names_from = REG_CD,
+      names_prefix = "ECLN_PRIX_REG_T\u00a7",
+      values_from = PVMM2_T_A
+    ) -> tab_reg_prix
+
+  readr::write_csv(tab_reg_prix,
+    here::here(
+      "4_resultats",
+      paste0("ECLN_PRIXM_REG_T_", fich_rd1_trim, "_", Sys.Date(), ".csv")
+    ),
+    append = FALSE
+  )
+
+  # tableau RD1 mis en forme pour importation
+  #
+
+  ls_tab2[["94"]] -> tab_cor
+
+  readr::write_csv(tab_cor,
+    here::here(
+      "4_resultats",
+      paste0("ECLN_tab_rd1_", fich_rd1_trim, "_", Sys.Date(), ".csv")
+    ),
+    append = FALSE
+  )
+}
+
---
+++ R/fn03_transforme_requete.R
@@ -0,0 +1,275 @@
+#' fn03_transforme_requete
+#'
+#' @param x le nom du fichier issu de geokit
+#'
+#' @importFrom attempt attempt
+#' @importFrom dplyr case_when
+#' @importFrom dplyr filter
+#' @importFrom dplyr group_by
+#' @importFrom dplyr inner_join
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr mutate_if
+#' @importFrom dplyr pull
+#' @importFrom dplyr rename
+#' @importFrom dplyr select
+#' @importFrom dplyr starts_with
+#' @importFrom dplyr summarise
+#' @importFrom dplyr tibble
+#' @importFrom janitor clean_names
+#' @importFrom purrr flatten_chr
+#' @importFrom purrr imap
+#' @importFrom purrr iwalk
+#' @importFrom purrr map_dfr
+#' @importFrom purrr set_names
+#' @importFrom purrr transpose
+#' @importFrom readr write_csv
+#' @importFrom rio export
+#' @importFrom stringr str_c
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_sub
+#' @importFrom tibble as_tibble
+#' @importFrom tibble tribble
+#' @importFrom tidyr expand_grid
+#' @importFrom tidyr pivot_longer
+#' @importFrom tidyr pivot_wider
+#' @importFrom tidyr replace_na
+#' @importFrom utf8 as_utf8
+#'
+#' @return nothing
+#' @export
+fn03_transforme_requete <- function(x = "Indicateurs_ecln_trim2022.xlsx") {
+
+  # chem_dir <- here("2_data")
+  # fich_nom <- list.files(chem_dir, pattern = ".xlsx$") %>% str_subset("2022")
+  adr_fich <- file.path(getwd(), "2_data", x)
+
+  # teste existence du fichier
+  attempt::attempt(
+    file.exists(adr_fich) == FALSE,
+    msg = utf8::as_utf8(paste0("Le fichier ", x, "n'est pas dans 2_data"))
+  )
+
+
+  if (file.exists(file.path(getwd(), "2_data", x))) {
+    excel_sheets(adr_fich) %>% purrr::set_names() -> fich_onglets
+
+    purrr::map(fich_onglets, ~ read_xlsx(adr_fich, .x) %>%
+      janitor::clean_names()) -> ls_onglets
+
+    names(ls_onglets) -> eff
+
+    # tibble(
+    #   champs0 = map(ls_onglets, ~ names(.x)) %>% flatten_chr() %>% unique() %>% sort(),
+    #   champs = c(
+    #     "g_com_cd",
+    #     "g_reg_cd",
+    #     "g_com_lib",
+    #     "g_reg_lib",
+    #     "lgt_enc",
+    #     "lgt_mev",
+    #     "lgt_res",
+    #     "mod_nbpieces",
+    #     "lgt_prixtot",
+    #     "lgt_surftot",
+    #     "dt_trimestre",
+    #     "mod_type"
+    #   )
+    # )-> t_champs
+    # write_csv(t_champs, here("3_tables", "t_champs.csv"))
+    tibble::tribble(
+      ~champs0, ~champs,
+      "code_de_la_commune", "g_com_cd",
+      "code_de_la_region", "g_reg_cd",
+      "libelle_de_la_commune", "g_com_lib",
+      "libelle_de_la_region", "g_reg_lib",
+      "nb_encours_lgts_proposes_a_la_vente", "lgt_enc",
+      "nb_lgt_mis_en_vente", "lgt_mev",
+      "nb_lgt_reserves_a_la_vente", "lgt_res",
+      "nombre_de_pieces", "mod_nbpieces",
+      "prix_total_des_ventes_euros", "lgt_prixtot",
+      "surface_totale_m2", "lgt_surftot",
+      "trimestre", "dt_trimestre",
+      "type_de_construction", "mod_type"
+    ) -> t_champs
+
+
+    purrr::map_dfr(ls_onglets, ~ dplyr::tibble(champs0 = names(.x)), .id = "onglet") %>%
+      dplyr::inner_join(t_champs, by = "champs0") -> t_champs0
+
+    # add a test !!!!!
+
+    purrr::map(
+      names(ls_onglets),
+      ~ ls_onglets[[.x]] %>%
+        purrr::set_names(t_champs0 %>% dplyr::filter(onglet %in% .x) %>% dplyr::pull(champs)) %>%
+        dplyr::mutate(
+          "trim_day" = dplyr::case_when(
+            stringr::str_sub(dt_trimestre, 5, 5) %in% "1" ~ "03-31",
+            stringr::str_sub(dt_trimestre, 5, 5) %in% "2" ~ "06-30",
+            stringr::str_sub(dt_trimestre, 5, 5) %in% "3" ~ "09-30",
+            stringr::str_sub(dt_trimestre, 5, 5) %in% "4" ~ "12-31",
+            TRUE ~ "Pb"
+          ),
+          "dt_date" = paste(stringr::str_sub(dt_trimestre, 1, 4), trim_day, sep = "-")
+        ) %>%
+        dplyr::select(-trim_day) %>%
+        dplyr::select(
+          dplyr::starts_with("g_"),
+          dplyr::starts_with("dt_"),
+          dplyr::starts_with("mod_"),
+          dplyr::starts_with("lgt_")
+        )
+    ) %>% purrr::set_names(eff) -> ls_onglets
+
+    purrr::map(ls_onglets, ~ .x %>%
+      dplyr::pull(dt_trimestre) %>%
+      max() %>%
+      as.character()) %>%
+      purrr::flatten_chr() %>%
+      unique() -> trim_dernier
+
+
+
+    # Requete nombre de pièces -----
+
+    ls_onglets$cor_pieces %>%
+      dplyr::mutate(
+        nb_pieces = dplyr::case_when(
+          stringr::str_sub(mod_nbpieces, 1, 1) == 1 ~ "t1",
+          stringr::str_sub(mod_nbpieces, 1, 1) == 2 ~ "t2",
+          stringr::str_sub(mod_nbpieces, 1, 1) == 3 ~ "t3",
+          stringr::str_sub(mod_nbpieces, 1, 1) == 4 ~ "t4",
+          stringr::str_sub(mod_nbpieces, 1, 1) == 5 ~ "t5",
+          stringr::str_sub(mod_nbpieces, 1, 1) == 6 ~ "t6"
+        )
+      ) %>%
+      dplyr::select(-lgt_enc) %>%
+      tidyr::pivot_longer(
+        cols = dplyr::starts_with("lgt_"),
+        names_to = "indic",
+        values_to = "valeur"
+      ) %>%
+      dplyr::group_by(dt_trimestre, dt_date, nb_pieces, indic) %>%
+      dplyr::summarise(valeur = sum(valeur)) -> ls_onglets$cor_nbpieces_trim
+
+    fn_extrait_tab_pieces <-
+      function(x = "lgt_mev", y = "nb_lgt_") {
+        ls_onglets$cor_nbpieces_trim %>%
+          dplyr::filter(indic %in% x) %>%
+          tidyr::pivot_wider(
+            names_from = nb_pieces,
+            names_prefix = y,
+            values_from = valeur
+          ) %>%
+          dplyr::select(dt_trimestre, dplyr::starts_with("nb_"), dt_date) %>%
+          dplyr::rename(c("trimestre" = "dt_trimestre", "date" = "dt_date")) -> df_pieces
+        return(df_pieces)
+      }
+
+
+    noms_tab_pieces <-
+      stringr::str_c(
+        c("ECLN_mev_type_lgt_", "ECLN_resv_type_lgt_"),
+        stringr::str_c(stringr::str_sub(trim_dernier, 1, 4), "T", stringr::str_sub(trim_dernier, 5, 5))
+      )
+
+    purrr::imap(
+      c("lgt_mev", "lgt_res") %>% purrr::set_names(c("nb_lgt_", "nb_resa_")),
+      fn_extrait_tab_pieces
+    ) %>% purrr::set_names(noms_tab_pieces) -> ls_pieces
+
+    purrr::iwalk(ls_pieces, ~ rio::export(.x, here("4_resultats", paste0(.y, ".csv"))))
+
+
+    # mises en vente par EPCI ------
+
+    tab_geo <- COGugaison::table_supracom_2020
+
+    # Sortir la fonction !!!
+    verifie_geo_manquant2 <- function(data = ls_onglets) {
+      ls_verif <- list()
+      if (exists("tab_geo") == FALSE) {
+        tab_geo <- COGugaison::table_supracom_2020
+      } # pb avec cette ligne
+
+      unique(tab_geo$CODGEO[tab_geo$REG %in% 94]) -> ls_verif$cor_com$liste
+      all(
+        purrr::map(ls_verif$cor_com$liste, ~ .x %in% data$cor_com$g_com_cd) %>% unlist() == TRUE
+      ) -> ls_verif$cor_com$valid
+
+      purrr::transpose(ls_verif) -> ls_verif
+
+      return(ls_verif)
+    }
+
+
+    verifie_geo_manquant2() -> ls_verif
+
+    # Sortir la fonction !!!
+    fn_complete_table_com <- function(data = ls_onglets$cor_com) {
+      tidyr::expand_grid(
+        g_com_cd = ls_verif$liste[["cor_com"]],
+        dt_date = unique(data[["dt_date"]])
+      ) -> df0
+      if (nrow(df0) == nrow(data)) {
+        x -> result
+      } else {
+        dplyr::left_join(df0, data, by = c("g_com_cd", "dt_date")) %>%
+          dplyr::mutate_if(
+            .predicate = is.numeric,
+            .funs = ~ tidyr::replace_na(.x, 0)
+          ) -> result
+      }
+      return(result)
+    }
+
+    fn_complete_table_com() %>%
+      dplyr::select(g_com_cd, dt_date, lgt_mev) -> ls_onglets$cor_epci
+
+
+    # regroupement des communes par EPCI
+    #
+
+    tab_geo %>%
+      dplyr::filter(REG %in% "94") %>%
+      tibble::as_tibble() %>%
+      dplyr::select(CODGEO, EPCI) %>%
+      dplyr::rename(c("g_com_cd" = "CODGEO", "g_epci_cd" = "EPCI")) -> tab_epci
+
+    ls_onglets$cor_epci %>%
+      dplyr::left_join(tab_epci, by = "g_com_cd") %>%
+      dplyr::group_by(g_epci_cd, dt_date) %>%
+      dplyr::summarise(lgt_mev = sum(lgt_mev)) %>%
+      tidyr::pivot_wider(
+        names_from = g_epci_cd,
+        names_prefix = "ECLN_MEV_EPCI_AG_T\u00a7",
+        values_from = lgt_mev
+      ) %>%
+      dplyr::rename(c("date" = "dt_date")) -> ls_onglets$cor_epci
+
+    readr::write_csv(
+      ls_onglets$cor_epci,
+      here(
+        "4_resultats",
+        paste0("ECLN_MEV_EPCI_AG_T_", Sys.Date(), ".csv")
+      )
+    )
+
+    ls_onglets$reg_coll %>%
+      dplyr::filter(stringr::str_detect(mod_type, "Collectif")) %>%
+      dplyr::mutate(prix = round(lgt_prixtot / lgt_surftot, 0)) %>%
+      dplyr::select(dt_date, g_reg_cd, prix) %>%
+      tidyr::pivot_wider(
+        names_from = g_reg_cd,
+        names_prefix = "ECLN_PRIXM_REG_T\u00a7",
+        values_from = prix
+      ) %>%
+      dplyr::rename(c("date" = "dt_date")) -> tab_reg_prix_appart
+
+
+    print("Les fichiers sont dans 4_resultats\n")
+  } else {
+    print("Copier le fichier xlsx dans 2_data et relancer/kniter le script\n")
+  }
+}
