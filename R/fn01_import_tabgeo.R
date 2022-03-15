17b9ec18ad47c96d34c6b6934fe680037cbb0d62
ggwinter <49496645+ggwinter@users.noreply.github.com>
2022-03-15 18:03:19 +0100

    nettoie le package

--- R/fn01_import_tabgeo.R
+++ R/fn01_import_tabgeo.R
@@ -6,6 +6,8 @@
 #' @export
 #'
 #' @examples
+#' # table des epci 2020
+#' fn01_import_tabgeo(x = "table_supracom_2020")
 fn01_import_tabgeo <- function(x = "table_supracom_2020"){
   eval(substitute(COGugaison::x))-> tab_geo
   return(tab_geo)
